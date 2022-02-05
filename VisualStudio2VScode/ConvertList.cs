7ee22eed5d97710803457c0f4cbdcac3b70ff7b0
10101523 <10101523@syntecclub.com.tw>
2022-02-05 10:04:53 +0800

    delete unused file

---
+++ VisualStudio2VScode/ConvertList.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VisualStudio2VScode
+{
+    [Serializable]
+    public class ConvertList
+    {
+        public List<ConvertJob> JobList;
+    }
+
+    public class ConvertJob
+    {
+
+    }
+}
